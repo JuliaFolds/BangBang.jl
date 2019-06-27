3e2de74f2072ce2c71145a1dcb741172bc11eeb6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-06-27 05:46:06 +0000

    build based on 1124f7c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BangBang.jl-1","page":"Home","title":"BangBang.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [BangBang]","category":"page"}]
+}
