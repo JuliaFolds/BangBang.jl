fe712ade81311fa3a9fece808ab15b6b28f24c01
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-01 11:26:35 -0700

    Fix test_universalidentity.jl; use UniversalIdentity.Id
    Using version:
    https://github.com/tkf/UniversalIdentity.jl/commit/9d231743e8bd88efc8e766280850ddeba1355a2f

--- test/test_universalidentity.jl
+++ test/test_universalidentity.jl
@@ -1,13 +1,13 @@
 module TestUniversalidentity
 
 include("preamble.jl")
-using UniversalIdentity: Identity, hasidentity
+using UniversalIdentity: Id, hasidentity
 
 @testset begin
     @test hasidentity(push!!)
     @test hasidentity(append!!)
-    @test push!!(Identity(push!!), 1) == [1]
-    @test append!!(Identity(append!!), [1]) == [1]
+    @test push!!(Id(push!!), 1) == [1]
+    @test append!!(Id(append!!), [1]) == [1]
 end
 
 end  # module
