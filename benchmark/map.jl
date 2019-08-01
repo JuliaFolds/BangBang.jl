bcd623bedd97ad9f631dd6d61a955a33bcf221b2
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-01 15:15:01 -0700

    Avoid allocation of Array{Union{}} in benchmark/map.jl

--- benchmark/map.jl
+++ benchmark/map.jl
@@ -1,8 +1,21 @@
 using BangBang
 
+struct UndefArray{N, T <: NTuple{N, Int}} <: AbstractArray{Union{}, N}
+    size::T
+end
+
+Base.size(arr::UndefArray) = arr.size
+Base.isassigned(::UndefArray, ::Integer...) = false
+
+function BangBang.NoBang._setindex(x::UndefArray{N}, v, I...) where N
+    ys = Array{typeof(v), N}(undef, x.size...)
+    ys[I...] = v
+    return ys
+end
+
 
 mapbb(f, xs::Array, inbounds::Val = Val(true)) =
-    __map!!(f, similar(xs, Union{}), xs, 1, inbounds)
+    __map!!(f, UndefArray(size(xs)), xs, 1, inbounds)
 
 
 @inline function __map!!(f, ys, xs, n, inbounds)
