823fce1913008bbab42a553580feca98907985af
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-03 17:57:26 -0700

    Directly import InitialValues; no at-require
    It improves import time of Transducers.jl (1.6 sec -> 0.9 sec).

--- src/BangBang.jl
+++ src/BangBang.jl
@@ -16,6 +16,7 @@ export
     setproperty!!,
     splice!!
 
+using InitialValues
 using LinearAlgebra
 using Requires
 
@@ -25,13 +26,11 @@ using .NoBang: ImmutableContainer
 include("core.jl")
 include("base.jl")
 include("linearalgebra.jl")
+include("initials.jl")
 
 function __init__()
     @require StaticArrays="90137ffa-7385-5640-81b9-e52037218182" begin
         include("staticarrays.jl")
-    end
-    @require InitialValues="22cec73e-a1b8-11e9-2c92-598750a2cf9c" begin
-        include("initials.jl")
     end
 end
 
