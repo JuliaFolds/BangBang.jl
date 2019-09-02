d324583d5253319afea2a1abd8f25f814aa16624
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-02 12:44:14 -0700

    Simplify RNN example

--- examples/zygote.jl
+++ examples/zygote.jl
@@ -1,16 +1,16 @@
 using BangBang
 using LinearAlgebra
 
-function rnn(n, J, x)
+function rnn!!(n, J, x)
+    dest = x
     y = similar(x)
-    local result
     for _ in 1:n
         @! y = mul!(y, J, x)
         @! y .= tanh.(y)
-        result = y
         x, y = y, x
     end
-    return result === x ? (@! y .= result) : result
+    @! dest .= x
+    return dest
 end
 
 using Zygote
@@ -18,4 +18,4 @@ d = 10
 J = randn(d, d)
 x0 = randn(d)
 y_target = randn(d)
-g, = Zygote.gradient(J -> sum((rnn(20, J, x0) .- y_target) .^ 2), J)
+g, = Zygote.gradient(J -> sum((rnn!!(20, J, x0) .- y_target) .^ 2), J)
