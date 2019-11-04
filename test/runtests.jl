9f6f11b307821af2dc704258c68693da9b1bc1ba
Takafumi Arakaki <aka.tkf@gmail.com>
2019-11-03 19:13:48 -0800

    Skip test_zygote.jl with Julia 1.0 to avoid segfaults

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,15 @@ using Test
     r"^test_.*\.jl$",
     file,
 ) !== nothing])
+
+    if VERSION < v"1.2-" && file == "test_zygote.jl"
+        @info "Skip $file for Julia $VERSION to avoid segfault"
+        # For example:
+        # https://travis-ci.com/tkf/BangBang.jl/builds/134791701
+        # https://travis-ci.com/tkf/BangBang.jl/builds/134795161
+        continue
+    end
+
     include(file)
 end
 
