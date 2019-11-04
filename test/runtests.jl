c51494930780415109c09c09b715800030efbd05
Takafumi Arakaki <aka.tkf@gmail.com>
2019-11-03 19:11:25 -0800

    Run JuliaFormatter on test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 module TestBangBang
 using Test
 
-@testset "$file" for file in sort([file for file in readdir(@__DIR__) if
-                                   match(r"^test_.*\.jl$", file) !== nothing])
+@testset "$file" for file in sort([file for file in readdir(@__DIR__) if match(
+    r"^test_.*\.jl$",
+    file,
+) !== nothing])
     include(file)
 end
 
