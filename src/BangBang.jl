84c6cd89f720b9d3215e1fa3927b0dc838fc8f2c
Takafumi Arakaki <aka.tkf@gmail.com>
2019-11-03 17:39:32 -0800

    Run JuliaFormatter on src/BangBang.jl

--- src/BangBang.jl
+++ src/BangBang.jl
@@ -3,22 +3,21 @@ module BangBang
 # Use README as the docstring of the module:
 @doc read(joinpath(dirname(@__DIR__), "README.md"), String) BangBang
 
-export
-    @!,
-    append!!,
-    delete!!,
-    empty!!,
-    lmul!!,
-    materialize!!,
-    mul!!,
-    pop!!,
-    popfirst!!,
-    push!!,
-    pushfirst!!,
-    rmul!!,
-    setindex!!,
-    setproperty!!,
-    splice!!
+export @!,
+       append!!,
+       delete!!,
+       empty!!,
+       lmul!!,
+       materialize!!,
+       mul!!,
+       pop!!,
+       popfirst!!,
+       push!!,
+       pushfirst!!,
+       rmul!!,
+       setindex!!,
+       setproperty!!,
+       splice!!
 
 using Base.Broadcast: materialize!
 using InitialValues
@@ -37,10 +36,10 @@ include("macro.jl")
 include("zygote.jl")
 
 function __init__()
-    @require StaticArrays="90137ffa-7385-5640-81b9-e52037218182" begin
+    @require StaticArrays = "90137ffa-7385-5640-81b9-e52037218182" begin
         include("staticarrays.jl")
     end
-    @require StructArrays="09ab397b-f2b6-538f-b94a-2f83cf4a842a" begin
+    @require StructArrays = "09ab397b-f2b6-538f-b94a-2f83cf4a842a" begin
         include("structarrays.jl")
     end
 end
