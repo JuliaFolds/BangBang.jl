7614cc02b2b11e47497610b5428aeb21995202b4
Takafumi Arakaki <aka.tkf@gmail.com>
2019-11-04 16:33:42 -0800

    Run JuliaFormatter on docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,13 @@
 using Documenter, BangBang
 
-makedocs(;
-    modules=[BangBang],
-    format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-    ],
-    repo="https://github.com/tkf/BangBang.jl/blob/{commit}{path}#L{line}",
-    sitename="BangBang.jl",
-    authors="Takafumi Arakaki <aka.tkf@gmail.com>",
+makedocs(
+    ;
+    modules = [BangBang],
+    format = Documenter.HTML(),
+    pages = ["Home" => "index.md",],
+    repo = "https://github.com/tkf/BangBang.jl/blob/{commit}{path}#L{line}",
+    sitename = "BangBang.jl",
+    authors = "Takafumi Arakaki <aka.tkf@gmail.com>",
 )
 
-deploydocs(;
-    repo="github.com/tkf/BangBang.jl",
-)
+deploydocs(; repo = "github.com/tkf/BangBang.jl")
