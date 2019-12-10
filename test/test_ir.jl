146fff8fa6ca1c7695aefb1f53f820aedafb013d
Takafumi Arakaki <aka.tkf@gmail.com>
2019-12-09 19:31:16 -0800

    Print CPU information using CpuId

--- test/test_ir.jl
+++ test/test_ir.jl
@@ -1,5 +1,12 @@
 module TestIR
 
+using CpuId
+
+show(stdout, "text/plain", cpuinfo())
+println()
+show(stdout, "text/plain", cpufeaturetable())
+println()
+
 using IRTest
 IRTest.@include("__test_ir.jl")
 
