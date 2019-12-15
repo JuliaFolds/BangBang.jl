948ce3406ec7d6c12d9f7067fbe8462959a2927d
Takafumi Arakaki <aka.tkf@gmail.com>
2019-12-15 09:29:37 -0800

    Test append!!(::Table, ::SingletonVector) properly

--- test/test_typedtables.jl
+++ test/test_typedtables.jl
@@ -16,7 +16,7 @@ using TypedTables
     @test tints == Table(a = [1, 3], b = [2, 4])
 end
 
-@testset "append!!(::Table, ::$(constructorof(typeof(src))))" for src in Any[
+@testset "append!!(::Table, ::$(constructorof(typeof(src)))) [widen]" for src in Any[
     [(a = 3.0, b = 4.0)],
     SingletonVector((a = 3.0, b = 4.0)),
 ]
@@ -26,6 +26,18 @@ end
         b = [2.0, 4.0],
     )
     @test tints == Table(a = [1], b = [2])
+end
+
+@testset "append!!(::Table, ::$(constructorof(typeof(src)))) [mutation]" for src in Any[
+    # Broken:
+    # [(a = 3, b = 4)],
+    SingletonVector((a = 3, b = 4)),
+]
+    tints = Table(a = [1], b = [2])
+    @test append!!(tints, src) === tints == Table(
+        a = [1, 3],
+        b = [2, 4],
+    )
 end
 
 @testset "append!!" begin
