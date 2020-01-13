e12cd1fbd2b4efcfb69e6ed5018763bdd6325123
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-13 00:14:27 -0800

    Test widening paths of modify!!(f, ::Dict, key)

--- test/test_modify.jl
+++ test/test_modify.jl
@@ -61,6 +61,60 @@ end
     end
 end
 
+@testset "modify!!(f, ::Dict, key)" begin
+    @testset "update" begin
+        dict = Dict("a" => 1)
+        backup = deepcopy(dict)
+        dict′, ret = modify!!(dict, "a") do old
+            Some(something(old, 0) + 0.5)
+        end
+        @test ret === Some(1.5)
+        @test dict′ == Dict("a" => 1.5)
+        @test dict == backup
+    end
+
+    @testset "insert" begin
+        dict = Dict{Union{},Union{}}()
+        backup = deepcopy(dict)
+        dict′, ret = modify!!(dict, "a") do old
+            Some(something(old, 0) + 1)
+        end
+        @test ret == Some(1)
+        @test dict′ == Dict("a" => 1)
+        @test dict == backup
+    end
+
+    @testset "no-op" begin
+        dict = Dict("a" => 1)
+        backup = deepcopy(dict)
+        dict′, ret = modify!!(_ -> nothing, dict, :b)
+        @test ret === nothing
+        @test dict′ === dict == backup
+    end
+
+    @testset "mutation inside `f` while widening value type" begin
+        dict = Dict{String,Int}()
+        dict′, ret = modify!!(dict, "a") do old
+            dict["a"] = 0
+            Some(something(old, 0) + 0.5)
+        end
+        @test ret === Some(0.5)
+        @test dict′ == Dict("a" => 0.5)
+        @test dict == Dict("a" => 0)
+    end
+
+    @testset "mutation inside `f` while widening key type" begin
+        dict = Dict{String,Int}()
+        dict′, ret = modify!!(dict, :b) do old
+            dict["a"] = 0
+            Some(something(old, 0) + 0.5)
+        end
+        @test ret === Some(0.5)
+        @test dict′ == Dict("a" => 0, :b => 0.5)
+        @test dict == Dict("a" => 0)
+    end
+end
+
 @testset "modify!!(f, ::NamedTuple, key)" begin
     @testset "update" begin
         @test modify!!((a = 1,), :a) do old
