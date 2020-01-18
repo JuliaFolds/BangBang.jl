d571d8c046f856212c8fc3fe48faae2d67e08e43
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-17 17:44:46 -0800

    Simplify _empty(::StaticArrays.StaticVector)

--- src/NoBang/staticarrays.jl
+++ src/NoBang/staticarrays.jl
@@ -5,10 +5,7 @@ pushfirst(xs::StaticArrays.StaticVector, y, ys...) =
 pop(xs::StaticArrays.StaticVector) = (StaticArrays.pop(xs), xs[end])
 popfirst(xs::StaticArrays.StaticVector) = (StaticArrays.popfirst(xs), xs[1])
 deleteat(xs::StaticArrays.StaticVector, i) = StaticArrays.deleteat(xs, i)
-_empty(xs::StaticArrays.StaticVector) =
-    foldl(ntuple(identity, length(xs)); init=xs) do xs, _
-        StaticArrays.pop(xs)
-    end
+_empty(xs::StaticArrays.StaticVector) = xs[StaticArrays.SOneTo(0)]
 _setindex(xs::StaticArrays.StaticArray, v, I...) = Base.setindex(xs, v, I...)
 
 StaticArrays.SArray(x::SingletonVector) = StaticArrays.SVector{1}(x)
