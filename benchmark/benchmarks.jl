bd7c87af2f4839f40d781e4526c44939c2f528a9
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-22 20:08:15 -0800

    Benchmark nothing

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,8 +1,3 @@
 using BenchmarkTools
 SUITE = BenchmarkGroup()
-for file in readdir(@__DIR__)
-    if startswith(file, "bench_") && endswith(file, ".jl")
-        SUITE[file[length("bench_") + 1:end - length(".jl")]] =
-            include(file)
-    end
-end
+SUITE["nothing"] = @benchmarkable nothing
