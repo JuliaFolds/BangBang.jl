a458369fb08286c8f42710f83c6b55632ce3a8df
Takafumi Arakaki <aka.tkf@gmail.com>
2020-06-30 19:39:52 -0700

    Push preview only when `push_preview` tag is specified

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,22 @@
-using Documenter, BangBang
+using BangBang
+using Documenter
+using JSON
+
+function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
+    event_path === nothing && return false
+    event = JSON.parsefile(event_path)
+    pull_request = get(event, "pull_request", nothing)
+    pull_request === nothing && return false
+    labels = [x["name"] for x in pull_request["labels"]]
+    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    yes = "push_preview" in labels
+    if yes
+        @info "Trying to push preview as label `push_preview` is specified." labels
+    else
+        @info "Not pushing preview as label `push_preview` is not specified." labels
+    end
+    return yes
+end
 
 makedocs(
     ;
@@ -14,4 +32,7 @@ makedocs(
     strict = true,
 )
 
-deploydocs(; repo = "github.com/JuliaFolds/BangBang.jl")
+deploydocs(;
+    repo = "github.com/JuliaFolds/BangBang.jl",
+    push_preview = should_push_preview(),
+)
