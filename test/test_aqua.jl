9f67369bbc6e6bbbf8faff68258d324163b010eb
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-07 18:23:21 -0700

    Use flags to control Aqua.test_all (#198)
    Use flag-based configuration added in Aqua 0.4.7.

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -8,10 +8,15 @@ using Test
 Aqua.test_all(
     BangBang;
     ambiguities=(exclude=[Base.get, Setfield.set, Setfield.modify],),
+    project_extras = true,
+    stale_deps = true,
+    deps_compat = true,
+    project_toml_formatting = true,
 )
 
-@testset "Stale dependencies" begin
-    Aqua.test_stale_deps(BangBang)
+@testset "Compare test/Project.toml and test/environments/main/Project.toml" begin
+    @test Text(read(joinpath(@__DIR__, "Project.toml"), String)) ==
+          Text(read(joinpath(@__DIR__, "environments", "main", "Project.toml"), String))
 end
 
 end  # module
