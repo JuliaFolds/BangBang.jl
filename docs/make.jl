14920b937c7794098443fa72e9adcd3ea1f23c1a
Takafumi Arakaki <aka.tkf@gmail.com>
2022-01-12 20:57:23 -0500

    Ignore warning about missing docs

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,24 @@ makedocs(
     sitename = "BangBang.jl",
     authors = "Takafumi Arakaki <aka.tkf@gmail.com>",
     doctest = false,  # run via tests
-    strict = true,
+    strict = [
+        :autodocs_block,
+        :cross_references,
+        :docs_block,
+        :doctest,
+        :eval_block,
+        :example_block,
+        :footnote,
+        :linkcheck,
+        :meta_block,
+        # Ignoring `:missing_docs` since Documenter does not handle docstrings
+        # defined outside the sub-module nicely:
+        # :missing_docs,
+        :parse_error,
+        :setup_block,
+    ],
+    # Ref:
+    # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.makedocs
 )
 
 deploydocs(; repo = "github.com/JuliaFolds/BangBang.jl", push_preview = true)
